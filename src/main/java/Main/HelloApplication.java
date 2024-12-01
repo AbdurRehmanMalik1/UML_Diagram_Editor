4a132eca968fdbb54536fe9f45317d69374f051f
Hadia-Sarwar <i222586@lhr.nu.edu.pk>
2024-12-02 04:56:40 +0500

    Opening Window UI, Styling done.

--- src/main/java/Main/HelloApplication.java
+++ src/main/java/Main/HelloApplication.java
@@ -21,8 +21,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/ClassDiagram-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        stage.setTitle("UML Diagrams!");
         stage.setScene(scene);
         stage.show();
     }
