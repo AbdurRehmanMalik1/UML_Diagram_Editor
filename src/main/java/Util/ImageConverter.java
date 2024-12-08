df74713212d794a0c7783214657e8d602432e8c9
AbdurRehmanMalik1 <abdurrehman4415@gmail.com>
2024-12-08 21:14:43 +0500

    adding Comments

--- src/main/java/Util/ImageConverter.java
+++ src/main/java/Util/ImageConverter.java
@@ -6,20 +6,31 @@ import javafx.scene.image.WritableImage;
 import java.awt.image.BufferedImage;
 
 public class ImageConverter {
-    public static BufferedImage convertToBufferedImage(WritableImage writableImage) {
-        int width = (int) writableImage.getWidth();
-        int height = (int) writableImage.getHeight();
 
+    /**
+     * Converts a JavaFX `WritableImage` to a `BufferedImage`.
+     *
+     * @param writableImage The JavaFX `WritableImage` to be converted.
+     * @return A `BufferedImage` representation of the `WritableImage`.
+     */
+    public static BufferedImage convertToBufferedImage(WritableImage writableImage) {
+        int width = (int) writableImage.getWidth(); // Get the width of the image
+        int height = (int) writableImage.getHeight(); // Get the height of the image
+
+        // Create a new BufferedImage with the same dimensions and ARGB format
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-        PixelReader pixelReader = writableImage.getPixelReader();
+        PixelReader pixelReader = writableImage.getPixelReader(); // Get the PixelReader from the WritableImage
 
+        // Iterate over each pixel in the WritableImage
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                // Get the ARGB value of the current pixel
                 int argb = pixelReader.getArgb(x, y);
+                // Set the ARGB value to the corresponding pixel in the BufferedImage
                 bufferedImage.setRGB(x, y, argb);
             }
         }
-        return bufferedImage;
+        return bufferedImage; // Return the converted BufferedImage
     }
 }
